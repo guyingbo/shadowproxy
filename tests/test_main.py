027f2689196bbd0172807660586a9233876e897e
Gu Yingbo <guyingbo@papayamobile.com>
2017-03-23 15:54:44 +0800

    more test cases

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,3 +4,14 @@ import shadowproxy
 def test_is_local():
     assert shadowproxy.is_local('127.0.0.1') is True
     assert shadowproxy.is_local('192.168.20.168') is True
+    assert shadowproxy.is_local('211.13.20.168') is False
+
+
+def test_pack_addr():
+    assert shadowproxy.pack_addr(('127.0.0.1', 8080)) == \
+            b'\x01\x7f\x00\x00\x01\x1f\x90'
+
+
+def test_unpack_addr():
+    addr = ('232.32.9.86', 49238)
+    assert shadowproxy.unpack_addr(shadowproxy.pack_addr(addr))[0] == addr
