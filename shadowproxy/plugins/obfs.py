9a88db7cb19e4e5505e2c2fee61c9b50f516e17b
Gu Yingbo <guyingbo@papayamobile.com>
2018-08-13 11:05:29 +0800

    http plugin

--- shadowproxy/plugins/obfs.py
+++ shadowproxy/plugins/obfs.py
@@ -8,14 +8,20 @@ class HttpSimplePlugin:
 
     async def run(self, client):
         buf = bytearray()
+        start = 0
         while True:
             data = await client.recv(gvars.PACKET_SIZE)
             if not data:
                 return
             buf.extend(data)
-            index = buf.find(b"\r\n\r\n")
+            index = buf.find(b"\r\n\r\n", start)
             if index == -1:
+                start = len(buf) - 3
+                start = start if start > 0 else 0
                 continue
+            head = bytes.fromhex(
+                buf.split(b" ", 2)[1][1:].replace(b"%", b"").decode("ascii")
+            )
             await client.sendall(
                 b"HTTP/1.1 200 OK\r\n"
                 b"Connection: keep-alive\r\n"
@@ -26,13 +32,12 @@ class HttpSimplePlugin:
                 + b"\r\nServer: nginx\r\n"
                 b"Vary: Accept-Encoding\r\n\r\n"
             )
-            redundant = memoryview(buf)[index + 4 :].tobytes()
-            if redundant:
-                recv = client.recv
+            redundant = head + memoryview(buf)[index + 4 :].tobytes()
+            recv = client.recv
 
-                async def tmp_recv(size):
-                    client.rev = recv
-                    return redundant
+            async def disposable_recv(size):
+                client.rev = recv
+                return redundant
 
-                client.recv = tmp_recv
+            client.recv = disposable_recv
             return
