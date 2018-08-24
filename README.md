688e66f03c84abcd3f179a95d0969dca30941f2c
Gu Yingbo <guyingbo@papayamobile.com>
2018-08-24 17:03:48 +0800

    doc

--- README.md
+++ README.md
@@ -14,40 +14,35 @@ A proxy server that implements Socks5/Shadowsocks/Redirect/HTTP (tcp) and Shadow
 
 Thanks to Dabeaz's awesome curio project: https://github.com/dabeaz/curio
 
-This project is inspired by qwj's python-proxy project(https://github.com/qwj/python-proxy), and some part of http proxy code was copy from it.
+This project is inspired by qwj's python-proxy project(https://github.com/qwj/python-proxy).
 
-
-It is a replacement of shadowsocks and shadowsocks-libev, you can replace ss-redir, ss-tunnel, ss-server, ss-local with shadowproxy.py
+It is a replacement of shadowsocks and shadowsocks-libev, you can replace ss-redir, ss-tunnel, ss-server, ss-local with just one shadowproxy command.
 
 ## Usage
 
 ```
 usage: shadowproxy [-h] [-v] [--version] [--monitor] server [server ...]
 
-uri syntax: {server_scheme}://[cipher:password@]{netloc}[#fragment][{=client_scheme}://[cipher:password@]{netloc}]
+uri syntax:
 
-support tcp schemes:
-  server_scheme: socks, ss, red, http, https
-  client_scheme: ss, http
-support udp schemes:
-  server_scheme: ssudp, tproxyudp, tunneludp
-  client_scheme: ssudp
+{scheme}://[{userinfo}@][hostname]:{port}[/?[plugin={p;args}][via={uri}][target={t}]][#{fragment}]
+
+userinfo = cipher:password or base64(cipher:password) when scheme is ss, ssudp
+userinfo = username:password or base64(username:password) when scheme is socks, http.
+
 ```
 
 examples:
 
 ```
 # socks5 --> shadowsocks
-shadowproxy -v socks://:8527=ss://aes-256-cfb:password@127.0.0.1:8888
+shadowproxy -v socks://:8527/?via=ss://aes-256-cfb:password@127.0.0.1:8888
 
 # http   --> shadowsocks
-shadowproxy -v http://:8527=ss://aes-256-cfb:password@127.0.0.1:8888
-
-# https  --> shadowsocks
-shadowproxy -v https://:8527#keyfile,certfile=ss://aes-256-cfb:password@127.0.0.1:8888
+shadowproxy -v http://:8527/?via=ss://aes-256-cfb:password@127.0.0.1:8888
 
 # redir  --> shadowsocks
-shadowproxy -v red://:12345=ss://aes-256-cfb:password@127.0.0.1:8888
+shadowproxy -v red://:12345/?via=ss://aes-256-cfb:password@127.0.0.1:8888
 
 # shadowsocks server (tcp)
 shadowproxy -v ss://aes-256-cfb:password@:8888
@@ -56,10 +51,10 @@ shadowproxy -v ss://aes-256-cfb:password@:8888
 shadowproxy -v ssudp://aes-256-cfb:password@:8527
 
 # tunnel --> shadowsocks (udp)
-shadowproxy -v tunneludp://:8527#8.8.8.8:53=ssudp://aes-256-cfb:password@127.0.0.1:8888
+shadowproxy -v tunneludp://:8527/?target=8.8.8.8:53&via=ssudp://aes-256-cfb:password@127.0.0.1:8888
 
 # tproxy --> shadowsocks (udp)
-shadowproxy -v tproxyudp://:8527=ssudp://aes-256-cfb:password@127.0.0.1:8888
+shadowproxy -v tproxyudp://:8527/?via=ssudp://aes-256-cfb:password@127.0.0.1:8888
 ```
 
 ## Installation
--- shadowproxy/__init__.py
+++ shadowproxy/__init__.py
@@ -1,47 +1,51 @@
 """
 An universal proxy server/client which support
-Socks5/HTTP/Shadowsocks/Redirect (tcp) and
+Socks/HTTP/Shadowsocks/Redirect (tcp) and
 Shadowsocks/TProxy/Tunnel (udp) protocols.
 
 uri syntax:
 
 {scheme}://[{userinfo}@][hostname]:{port}[/?[plugin={p;args}][via={uri}][target={t}]][#{fragment}]
 
-userinfo = base64(cipher:password) when scheme is ss, ssudp
-userinfo = base64(username:password) when scheme is socks, http or https.
+userinfo = cipher:password or base64(cipher:password) when scheme is ss, ssudp
+userinfo = username:password or base64(username:password) when scheme is socks, http.
 
-support tcp schemes:
-  scheme:   socks, ss, red, http, https
-  via_scheme:  ss
-support udp schemes:
-  scheme:   ssudp, tproxyudp, tunneludp
-  via_scheme:  ssudp
+supported protocols:
+
+protocol        server  client  scheme
+socks5          yes     yes     socks://
+socks4          yes     yes     socks4://
+ss              yes     yes     ss://
+ss aead         yes     yes     ss://
+http            yes     yes     httponly://
+http connect    yes     yes     http://
+transparent     yes     no      red://
 
 examples:
-  # http(s) proxy
-  shadowproxy -v http://:8527
 
-  # socks5 --> shadowsocks
-  shadowproxy -v socks://:8527/?via=ss://aes-256-cfb:password@127.0.0.1:8888
+# http(s) proxy
+shadowproxy -v http://:8527
 
-  # http   --> shadowsocks
-  shadowproxy -v http://:8527/?via=ss://aes-256-cfb:password@127.0.0.1:8888
+# socks5 -> shadowsocks
+shadowproxy -v 'socks://:8527/?via=ss://aes-256-cfb:password@127.0.0.1:8888'
 
-  # redir  --> shadowsocks
-  shadowproxy -v red://:12345/?via=ss://aes-256-cfb:password@127.0.0.1:8888
+# http   -> shadowsocks
+shadowproxy -v 'http://:8527/?via=ss://aes-256-cfb:password@127.0.0.1:8888'
 
-  # shadowsocks server (tcp)
-  shadowproxy -v ss://aes-256-cfb:password@:8888
+# redir  -> shadowsocks
+shadowproxy -v 'red://:12345/?via=ss://aes-256-cfb:password@127.0.0.1:8888'
 
-  # shadowsocks server (udp)
-  shadowproxy -v ssudp://aes-256-cfb:password@:8527
+# shadowsocks server (tcp)
+shadowproxy -v ss://aes-256-cfb:password@:8888
 
-  # tunnel --> shadowsocks (udp)
-  shadowproxy -v \
-tunneludp://:8527/?target=8.8.8.8:53&via=ssudp://aes-256-cfb:password@127.0.0.1:8888
+# shadowsocks server (udp)
+shadowproxy -v ssudp://aes-256-cfb:password@:8527
 
-  # tproxy --> shadowsocks (udp)
-  sudo shadowproxy -v \
-tproxyudp://:8527/?via=ssudp://aes-256-cfb:password@127.0.0.1:8888
+# tunnel -> shadowsocks (udp)
+shadowproxy -v \
+    tunneludp://:8527/?target=8.8.8.8:53&via=ssudp://chacha20:pass@127.0.0.1:8888
+
+# tproxy -> shadowsocks (udp)
+sudo shadowproxy -v tproxyudp://:8527/?via=ssudp://chacha20:password@127.0.0.1:8888
 """
 __version__ = "0.4.0"
